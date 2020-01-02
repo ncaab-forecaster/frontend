d8d225e0460f05ad887ae95f86ddca918609b733
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-01-02 14:50:32 -0500

    ready to start Compare comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import homebasket from "../homebasket.jpg";
 import Layout from "antd/es/layout";
 import "antd/es/layout/style/css";
@@ -14,16 +14,13 @@ const Home = () => {
     <Layout className="layout">
       <Content style={{ padding: "0 50px" }}>
         <Breadcrumb style={{ margin: "24px 0" }}>
-          {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item> */}
         </Breadcrumb>
         <div className='home-div' style={{ background: "#fff", padding: 24, minHeight: "80vh" }}>
           <h1 className='home-title'>We Predict Scores</h1>
           <h3 className='home-text'>
             NCAAB Predictor uses historical data and advanced Data Science to
             predict Division I NCAA basketball scores. You can select any two
-            teams in <Link to="/compare">Compare</Link> or check out the predictions for today's games in <Link to="/today"> Today</Link>.
+            teams in <NavLink to="/compare">Compare</NavLink> or check out the predictions for today's games in <NavLink to="/today"> Today</NavLink>.
           </h3>
           <img
             className="main-image"

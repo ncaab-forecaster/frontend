b38bc8131117a96226e1632dd77f3af43d66df73
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-01-02 14:40:32 -0500

    Home finished for now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,38 @@
 import React from "react";
-import Layout from 'antd/es/layout'
-import 'antd/es/layout/style/css'
-import Breadcrumb from 'antd/es/breadcrumb'
-import 'antd/es/breadcrumb/style/css'
+import { Link } from "react-router-dom";
+import homebasket from "../homebasket.jpg";
+import Layout from "antd/es/layout";
+import "antd/es/layout/style/css";
+import Breadcrumb from "antd/es/breadcrumb";
+import "antd/es/breadcrumb/style/css";
+import "../styles/Home.css";
 
 const { Content } = Layout;
 
 const Home = () => {
-
   return (
     <Layout className="layout">
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '24px 0' }}>
-        {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb style={{ margin: "24px 0" }}>
+          {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
         <Breadcrumb.Item>List</Breadcrumb.Item>
         <Breadcrumb.Item>App</Breadcrumb.Item> */}
-      </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: '80vh' }}>Home</div>
-    </Content>
-  </Layout>
+        </Breadcrumb>
+        <div className='home-div' style={{ background: "#fff", padding: 24, minHeight: "80vh" }}>
+          <h1 className='home-title'>We Predict Scores</h1>
+          <h3 className='home-text'>
+            NCAAB Predictor uses historical data and advanced Data Science to
+            predict Division I NCAA basketball scores. You can select any two
+            teams in <Link to="/compare">Compare</Link> or check out the predictions for today's games in <Link to="/today"> Today</Link>.
+          </h3>
+          <img
+            className="main-image"
+            src={homebasket}
+            alt="black and white basket"
+          />
+        </div>
+      </Content>
+    </Layout>
   );
 };
 
